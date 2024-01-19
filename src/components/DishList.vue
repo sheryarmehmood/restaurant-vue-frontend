59234c3eec9dc15c3077e7c42ab7d0496ebd1780
sheryarmehmood <63617629+sheryarmehmood@users.noreply.github.com>
2024-01-20 03:23:53 +0500

    dish list pagination done

--- src/components/DishList.vue
+++ src/components/DishList.vue
@@ -25,7 +25,7 @@
       </table>
   
       <!-- Display Pagination Links -->
-      <nav aria-label="Page navigation">
+      <!-- <nav aria-label="Page navigation">
         <ul class="pagination">
           <li class="page-item" :class="{ 'disabled': !dishes.prev_page_url }">
             <a class="page-link" @click.prevent="fetchDishes(dishes.prev_page_url)" href="#" aria-label="Previous">
@@ -43,7 +43,58 @@
             </a>
           </li>
         </ul>
-      </nav>
+      </nav> -->
+      <!-- Display Pagination Links -->
+    <!-- <nav aria-label="Page navigation">
+      <ul class="pagination">
+        <li class="page-item" :class="{ 'disabled': !links.prev_page_url }">
+          <a class="page-link" @click.prevent="fetchDishes(links.current_page - 1)" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+
+        <li class="page-item" v-for="page in links.last_page" :key="page" :class="{ 'active': page === links.current_page }">
+          <a class="page-link" @click.prevent="fetchDishes(page)" href="#">{{ page }}</a>
+        </li>
+
+        <li class="page-item" :class="{ 'disabled': !links.next_page_url }">
+          <a class="page-link" @click.prevent="fetchDishes(links.current_page + 1)" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav> -->
+
+
+     <!-- Display Pagination Links -->
+     <nav aria-label="Page navigation">
+      <ul class="pagination">
+        <li class="page-item" :class="{ 'disabled': !links.prev_page_url }">
+          <a v-if="links.prev_page_url" class="page-link" @click.prevent="fetchDishes(links.prev_page_url)" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+          <span v-else class="page-link" aria-label="Previous" disabled>
+            <span aria-hidden="true">&laquo;</span>
+          </span>
+        </li>
+
+        <li class="page-item" v-for="page in links.last_page" :key="page" :class="{ 'active': page === links.current_page }">
+          <a class="page-link" @click.prevent="fetchDishes(`${links.path}?page=${page}`)" href="#">{{ page }}</a>
+        </li>
+
+
+        <li class="page-item" :class="{ 'disabled': !links.next_page_url }">
+          <a v-if="links.next_page_url" class="page-link" @click.prevent="fetchDishes(links.next_page_url)" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+          <span v-else class="page-link" aria-label="Next" disabled>
+            <span aria-hidden="true">&raquo;</span>
+          </span>
+        </li>
+      </ul>
+    </nav>
+
+
     </div>
   </template>
   
@@ -54,6 +105,7 @@
     data() {
       return {
         dishes: [],
+        links: [],
       };
     },
     mounted() {
@@ -69,8 +121,13 @@
         },
       })
           .then(response => {
-            console.log('response',response);
+            // console.log('response',response);
             this.dishes = response.data.data.data;
+            this.links = response.data.data;
+            // console.log('this.links',this.links);
+            console.log('response.data.data',response.data.data);
+
+
           })
           .catch(error => {
             console.error('Error fetching dishes:', error);
