26ca85b7af21ded8560c0ac503761ab51ed25a46
sheryarmehmood <63617629+sheryarmehmood@users.noreply.github.com>
2024-01-19 17:37:10 +0500

    routing has history changed to web hohstory to remove hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HelloWorld from '@/components/HelloWorld.vue';
 import LoginView from '@/views/LoginView.vue';
 
@@ -9,7 +9,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
